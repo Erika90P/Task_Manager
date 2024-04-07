a0c6f6843cac2ddf266be4c535733c7c03c5a724
Erika90 <erikilla.pinzon@email.com>
2024-04-07 14:33:00 -0400

    configuration of the port

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
+import Express from "express"
+const app = Express()
+const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.json("this is the 'index' index route, your server is running!")
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`you are listening on port http://localhost:${PORT}`)
+})
